700d6bcfc7177f0a5c3831bbd1463963be447234
Priscilla201 <priscillafcosta20@gmail.com>
2020-01-23 10:44:11 -0300

    Projeto criado

---
+++ src/main/java/com/curso/cursonew/CursoNew1Application.java
@@ -0,0 +1,13 @@
+package com.curso.cursonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNew1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNew1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/cursonew/CursoNew1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.cursonew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNew1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
